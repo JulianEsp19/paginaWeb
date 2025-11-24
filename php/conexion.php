1a3c8ca1fc109acb6b1dc2adba2c308e27468313
Julian Esparza <117664575+JulianEsp19@users.noreply.github.com>
2025-11-24 13:46:24 -0600

    Update conexion.php

--- php/conexion.php
+++ php/conexion.php
@@ -1,5 +1,5 @@
 <?php
-$server = "dpg-d4iak9re5dus7385ktpg-a";
+$server = "postgresql://web_data_base_23110304_user:DLesf9WDIIwhw9Lan77mJBt0drjjw76h@dpg-d4iak9re5dus7385ktpg-a/web_data_base_23110304";
 $user = "web_data_base_23110304_user";
 $db = "web_data_base_23110304";
 $passwd = "DLesf9WDIIwhw9Lan77mJBt0drjjw76h";
