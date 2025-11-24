1c5ecb3cbace96ef39501d9dd912c7118ccf6c7e
Julian Esparza <117664575+JulianEsp19@users.noreply.github.com>
2025-11-24 15:28:01 -0600

    Update conexion.php

--- php/conexion.php
+++ php/conexion.php
@@ -5,10 +5,12 @@ $user = "web_data_base_23110304_user";
 $passwd = "DLesf9WDIIwhw9Lan77mJBt0drjjw76h";
 $port = "5432";
 
-$connection_string = "host=$host port=$port dbname=$db user=$user password=$passwd sslmode=require sslrootcert=/etc/ssl/certs/ca-certificates.crt";
+$connection_string = "host=$host port=$port dbname=$db user=$user password=$passwd sslmode=require";
 
 $con = pg_connect($connection_string);
 
 if (!$con) {
     echo "Conexion fallida";
+} else {
+    echo "Conexion exitosa";
 }
