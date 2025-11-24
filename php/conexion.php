dc24cb9d337488fc66bacd75a2bb36e5407cfdad
Julian Esparza <117664575+JulianEsp19@users.noreply.github.com>
2025-11-24 14:42:05 -0600

    Update conexion.php

--- php/conexion.php
+++ php/conexion.php
@@ -1,10 +1,11 @@
 <?php
 $server = "postgresql://web_data_base_23110304_user:DLesf9WDIIwhw9Lan77mJBt0drjjw76h@dpg-d4iak9re5dus7385ktpg-a/web_data_base_23110304";
 $user = "web_data_base_23110304_user";
+$port = "5432";
 $db = "web_data_base_23110304";
 $passwd = "DLesf9WDIIwhw9Lan77mJBt0drjjw76h";
 
-$con = pg_connect("host=$server dbname=$db user=$user password=$passwd");
+$con = pg_connect("host=$server port=$port dbname=$db user=$user password=$passwd sslmode=require");
 
 if($con){
     // echo("Conexion exitosa");
